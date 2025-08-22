bc426d1e3ab626fe2beac0a891a914fb8657cf0c
Dev Plugimmo <devplugimmo@MacBook-Pro-de-Dev.local>
2025-08-22 09:18:10 +0200

    Point 2: Ajout énumération status (draft/published) dans Article schema

--- api/articles/articles.schema.js
+++ api/articles/articles.schema.js
@@ -3,6 +3,14 @@ const { Schema, model } = require("mongoose");
 const articleSchema = Schema({
   title: String,
   content: String,
+  status: {
+    type: String,
+    enum: {
+      values: ["draft", "published"],
+      message: "{VALUE} n'est pas un statut valide"
+    },
+    default: "draft"
+  },
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
@@ -12,14 +20,3 @@ const articleSchema = Schema({
 let Article;
 
 module.exports = Article = model("Article", articleSchema);
-
-/*async function test() {
-  const articles = await Article.find().populate({
-    path: "user",
-    select: "-password",
-    match: { name: /ben/i },
-  });
-  console.log(articles.filter((article) => article.user));
-}
-
-test();*/
